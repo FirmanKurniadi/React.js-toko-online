48116c2093c06e87e495dae31aa0614595081689
FirmanKurniadi <57855120+FirmanKurniadi@users.noreply.github.com>
2019-11-27 20:49:17 +0800

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function App() {
+  return (
+    <React.Fragment>
+      <h3>Ini Halaman App</h3>
+    </React.Fragment>
+    
+  );
+}
+
+export default App;
---
+++ src/component/productlist.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class productlist extends Component {
+    render() {
+        return (
+            <div>
+                <h3>ini halaman produtlist</h3>
+            </div>
+        );
+    }
+}
+
+export default productlist;
